b21d25276ffa9f764b10abf9229ce4394bcfc0fb
ankitparmar2006 <ankitparmar30202@gmail.com>
2024-12-26 22:51:40 +0530

    update

--- about.js
+++ about.js
@@ -1,6 +1,29 @@
 
 //<!--====================  about  cording start from here   ===================== -->
 
+// <!--==================== about hide div  cording start from here   ===================== -->
+
+
+let div1=document.querySelector(".about-box9 ");
+let div2=document.querySelector(".about-box10 ");
+let div3=document.querySelector(".about-box11 ");
+let div4=document.querySelector(".about-box12 ");
+
+
+div1.addEventListener("click" , function (){
+    if (div1.style.display==="none" )
+        {
+            div1.style.display="block" 
+        }
+        else {
+            div1.style.display="none" 
+     
+        }
+})
+
+
+
+// <!--==================== about  hide div cording end from here   ===================== -->
 
 
 
